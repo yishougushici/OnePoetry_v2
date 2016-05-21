d0d1545d23acd4580e43209266e6b13fca4d8a21
Jason Song <wolfogre@outlook.com>
2016-05-21 17:25:37 +0800

    完成诗词找错后台

--- src/gushici/action/game/WrongAction.java
+++ src/gushici/action/game/WrongAction.java
@@ -7,6 +7,7 @@ import gushici.domain.Answer;
 import gushici.domain.User;
 import gushici.domain.Wrong;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
@@ -14,6 +15,28 @@ import java.util.Random;
  * 诗词游戏
  */
 public class WrongAction extends ActionSupport {
+	private Map<String, Object> dataMap;
+	private Integer serr_err_location;
+
+	public Map<String, Object> getDataMap() {
+		return dataMap;
+	}
+
+	public void setDataMap(Map<String, Object> dataMap) {
+		this.dataMap = dataMap;
+	}
+
+	public Integer getSerr_err_location() {
+		return serr_err_location;
+	}
+
+	public void setSerr_err_location(Integer serr_err_location) {
+		this.serr_err_location = serr_err_location;
+	}
+
+	public WrongAction() {
+		this.dataMap = new HashMap<>();
+	}
 
 	@Override
 	public String execute() throws Exception {
@@ -25,6 +48,60 @@ public class WrongAction extends ActionSupport {
 		actionContext.getSession().put("user", user);
 		actionContext.put("score", user.getSuesr__serr_score());
 		actionContext.put("pageTitle", "诗词找错");
+		return SUCCESS;
+	}
+
+	public String getQuestion() throws Exception  {
+		dataMap.clear();
+		ActionContext actionContext = ActionContext.getContext();
+		actionContext.getSession().remove("wrong");
+
+		HibernateTool hibernateTool = new HibernateTool();
+		Random random = new Random();
+		int countRecord = hibernateTool.count(Wrong.class);
+
+		int count = 0;
+		Wrong wrong = new Wrong();
+		while(++count < 20){
+			wrong = (Wrong)hibernateTool.get(Wrong.class, random.nextInt(countRecord));
+			if(wrong != null)
+				break;
+		}
+
+		if(wrong == null){
+			dataMap.put("error","服务器故障");
+			return SUCCESS;
+		}
+
+		dataMap.put("serr_content",wrong.getSerr_content().toCharArray());
+		dataMap.put("serr_sf_title",wrong.getSerr_sf_title());
+		dataMap.put("serr_sf_auth",wrong.getSerr_sf_auth());
+
+		actionContext.getSession().put("wrong", wrong);
+
+		return SUCCESS;
+	}
+
+	public String submitAnswer() throws Exception  {
+		dataMap.clear();
+		ActionContext actionContext = ActionContext.getContext();
+		Wrong wrong = (Wrong)actionContext.getSession().get("wrong");
+		User user = (User)actionContext.getSession().get("user");
+
+		if (wrong != null && wrong.getSerr_err_location().equals(getSerr_err_location())){
+			user.setSuesr__serr_score(user.getSuesr__serr_score() + 1);
+			HibernateTool hibernateTool = new HibernateTool();
+			hibernateTool.update(user);
+			actionContext.getSession().put("user", user);
+			actionContext.getSession().remove("wrong");
+			dataMap.put("result", true);
+			dataMap.put("score", user.getSuesr__serr_score());
+			return SUCCESS;
+		}
+
+		dataMap.put("result", false);
+		dataMap.put("score", user.getSuesr__serr_score());
+
 		return SUCCESS;
 	}
 }
